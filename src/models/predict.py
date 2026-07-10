175b2fc2392e28e9dc6e577c86415980dff61a44
ankita nayak <ankitaprasannanayak@gmail.com>
2026-07-10 09:26:11 +0530

    Update predict.py

--- src/models/predict.py
+++ src/models/predict.py
@@ -1,1 +1,7 @@
+# src/models/predict.py (Dummy version for Person 4 to use immediately)
+import numpy as np
 
+def predict_image(image_path):
+    # TODO: Person 2 will replace this with the real PyTorch model later
+    print(f"Fake predicting for: {image_path}")
+    return {"Normal": 0.1, "Pneumonia": 0.8, "Tuberculosis": 0.1}
