964f15cb674f9951e3fc3de9b0e61df785368651
Pratheeksha Nayak <pratheekshanayak7795@gmail.com>
2026-07-13 16:02:32 +0530

    Add FastAPI backend and prediction API

--- api/route.py
+++ api/route.py
@@ -1,1 +1,42 @@
+from fastapi import APIRouter, UploadFile, File, HTTPException
+from src.models.predict import predict_image
+import os
+import shutil
 
+router = APIRouter()
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+
+
+@router.get("/health")
+def health():
+    return {
+        "status": "Backend Working"
+    }
+
+
+@router.post("/predict")
+async def predict(file: UploadFile = File(...)):
+
+    # Check if uploaded file is an image
+    if file.content_type not in ["image/jpeg", "image/png", "image/jpg"]:
+        raise HTTPException(
+            status_code=400,
+            detail="Please upload a JPG or PNG image."
+        )
+
+    # Save uploaded image
+    file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+
+    with open(file_path, "wb") as buffer:
+        shutil.copyfileobj(file.file, buffer)
+
+    # Call Person 2's prediction function
+    result = predict_image(file_path)
+
+    # Send prediction back
+    return {
+        "filename": file.filename,
+        "prediction": result
+    }
