d8023cb04dd888b226523c7a2bffea3343868dfb
Pratheeksha Nayak <pratheekshanayak7795@gmail.com>
2026-07-18 13:33:25 +0530

    Integrate Streamlit frontend with FastAPI backend

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import requests
 
 st.set_page_config(
     page_title="LungLens AI",
@@ -23,3 +24,28 @@ uploaded_file = st.file_uploader(
 
 if uploaded_file is not None:
     st.success("Image uploaded successfully!")
+
+    files = {
+        "file": (
+            uploaded_file.name,
+            uploaded_file.getvalue(),
+            uploaded_file.type
+        )
+    }
+
+    response = requests.post(
+        "http://127.0.0.1:8000/predict",
+        files=files
+    )
+
+    if response.status_code == 200:
+       result = response.json()
+
+       prediction = result["prediction"]
+
+       st.subheader("Prediction")
+
+       st.success(f"Disease: {prediction[0]}")
+       st.write(f"Confidence: {prediction[1] * 100:.2f}%")
+    else:
+        st.error("Prediction failed.")
